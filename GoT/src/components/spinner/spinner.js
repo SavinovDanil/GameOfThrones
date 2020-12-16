51f7e2c2c379aca979d819ceaa32b2c9d0a1fe08
SavinovDanil <savinov.danil18@outlook.com>
2020-12-16 21:46:34 +0300

    first

---
+++ GoT/src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-rolling-szp7wu351b">
+            <div className="ldio-ioaibwqzrwh">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
